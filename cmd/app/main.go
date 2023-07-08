0a85d36d8edbc4ef1b908a066726b5c4b1c3c23f
vier21 <pilyaixavier21@gmail.com>
2023-07-08 13:11:29 +0700

    repair main

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,31 +1,16 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"log"
-
-	"github.com/vier21/go-book-api/pkg/services/user/model"
+	"github.com/vier21/go-book-api/pkg/db"
 	"github.com/vier21/go-book-api/pkg/services/user/repository"
+	"github.com/vier21/go-book-api/pkg/services/user/server"
 	"github.com/vier21/go-book-api/pkg/services/user/service"
 )
 
 func main() {
-	testup := repository.NewRepository()
-	testsvc := service.NewUserService(testup)
-	res , err := testsvc.UpdateUser(context.Background(),"8567c0e9-651c-4df0-999e-4ea37d14e7c3", model.UpdateUser{
-		Email: "kucing22@gmail.com",
-	})
-	
-	if err != nil {
-		log.Println(err)
-		return
-	}
 
-	fmt.Println(res)
-
-	// svc := service.NewUserService(repository.NewRepository())
-	// defer db.Disconnect()
-	// s := server.NewServer(svc)
-	// s.Run()
+	svc := service.NewUserService(repository.NewRepository())
+	defer db.Disconnect()
+	s := server.NewServer(svc)
+	s.Run()
 }
